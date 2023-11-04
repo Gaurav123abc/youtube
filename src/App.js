a4ced6b195233c036d53eecf7f10113a2aaa9a21
Astro_bot <autobot268@gmail.com>
2023-11-04 12:46:37 +0530

    react-route-dom package add and Routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Comp from './Comp';
 import Formcomponent from './form.js';
 import Boom_boom from './bim_bam_boom.js';
 import Click from './click.js';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
@@ -58,6 +60,26 @@ function App() {
         <Formcomponent></Formcomponent> */}
         <Boom_boom></Boom_boom>
         <Click></Click>
+
+{/* Routing in React npm install react-router-dom */}
+
+        <BrowserRouter>
+        
+          <Routes>
+
+            <Route path="/click" element={<Click />} />
+            <Route path="/boom" element={<Boom_boom />} />
+
+
+
+          </Routes>
+      
+        </BrowserRouter>
+
+
+
+
+
       </header>
       
     </div>
